6d2fe0326e3cd6e32cb974e244e3c59d744a0681
Monita1234 <nayibemontealegre@hotmail.com>
2015-06-30 18:45:46 -0500

    framework
    cambio

--- pecueshoes/pecueshoes/settings.py
+++ pecueshoes/pecueshoes/settings.py
@@ -126,7 +126,7 @@ INSTALLED_APPS = (
     # Uncomment the next line to enable admin documentation:
     'django.contrib.admindocs',
     'pecueshoes.apps.zapatos',#aplicacion de ventas
-    'rest_framework',
+    #'rest_framework',
     'gunicorn',
 )
 
